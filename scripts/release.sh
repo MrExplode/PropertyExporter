489f9a63dba0070d0a4890671a4f0cce9a0c0d17
MrExplode <pjanos@outlook.hu>
2019-04-25 15:22:33 +0200

    Corrected pom and release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,3 +1,4 @@
 cd ..
-mvn clean release:prepare -P release -Dgpg.passphrase=$GPG_PASSPHRASE
-mvn release:perform -P release -Dgpg.passphrase=$GPG_PASSPHRASE
+mvn clean release:prepare -P release -Dgpg.passphrase=$GPG_PASSPHRASE -Dgithub.token=$GITHUB_TOKEN
+mvn release:perform -P release -Dgpg.passphrase=$GPG_PASSPHRASE -Dgithub.token=$GITHUB_TOKEN
+read -n 1 -s -r -p "Press any key to continue"
